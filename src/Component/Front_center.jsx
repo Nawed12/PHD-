1c746a5fda6d452e26492a41a9e3b04baa34b144
Nawed <nawedhero786@gmail.com>
2023-01-31 22:57:51 +0530

    header home

---
+++ src/Component/Front_center.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../App.css'
+
+function Front_center(){
+    return(
+      <div>
+        <div className="image_home">
+  
+     
+  <div className="pic1">   </div> 
+  <div className="pic2">   </div> 
+  <div className="pic3">   </div> 
+  <div className="pic4">   </div> 
+  <div className="pic5">   </div> 
+  
+  
+  </div>
+  
+  
+  <div className="messages">
+  
+  <div className="content_of_message">
+   {/* <span className="message_heading">VICE-CHANCELLOR'S MESSAGE <br/></span> */}
+   <strong>Centurion University of Technology and Management (CUTM), Bhubaneswar</strong> is the first Multi-Sector Private State University in Odisha established by Odisha Act 4 of 2010. Centurion University has pioneered a unique model for delivering Industry aligned Skill Integrated Higher Education. This model has been widely recognized globally by United Nations, World Bank, Niti Ayog, Central and State Governments as well as leading firms such as EY.
+
+Centurion University is the only university in India to be recognized as a Center of Excellence for its School of Vocational Education and Training (Gazette No. F. SD-17/123/2019-SA/MSDE). The diverse student body, top-notch faculty from across India and abroad, industry partnerships and industrial facilities on campus ensure that Centurion University delivers on student aspirations, Employability, Entrepreneurship and/or Higher Studies. Centurion University has two campuses which are as follows:
+<br />
+<ul>
+  <li>
+Centurion University of Technology and Management, Paralkhemundi – Read more 
+</li>
+<br />
+<li>
+Centurion University of Technology and Management, Visakhapatnam – Read more
+</li>
+</ul>
+  </div>
+  
+  
+
+  
+  </div>
+  
+      </div>
+    )
+  }
+  
+  export default Front_center;
