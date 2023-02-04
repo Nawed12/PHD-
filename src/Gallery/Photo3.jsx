37b6869514ed2516e71d2c1bd98dd569c3bc1bc7
Bivu <bibhutibhusanpanda476@gmail.com>
2023-02-04 11:54:01 +0530

    Gallery

---
+++ src/Gallery/Photo3.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+class Photo3 extends React.Component {
+    render() {
+        return (
+            <div>
+                 
+                 <div className="gallery_pics"> 
+
+            
+
+
+
+
+
+                  
+<div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly1.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/poly1.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly2.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/poly2.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly3.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/poly3.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly4.jpg"} target="_blank">                
+    <img src={process.env.PUBLIC_URL+"/cutm/poly4.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/7.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/7.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+    
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly3.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/poly3.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/poly4.jpg"} target="_blank">                
+    <img src={process.env.PUBLIC_URL+"/cutm/poly4.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+    <div className="gallery">
+    <a href={process.env.PUBLIC_URL+"/cutm/8.jpg"} target="_blank">
+    <img src={process.env.PUBLIC_URL+"/cutm/8.jpg"} alt="Cinque Terre" width="600" height="400"/></a>
+    <div className="desc">CUTM BUILDING</div>
+    </div>
+
+
+
+</div>
+
+
+            </div>
+        )
+    }
+}
+export default Photo3;
